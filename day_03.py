7e1417c6d6fae27e7db99ff8b51a139250c231f3
David Williams <dbw16@hotmail.com>
2022-12-03 09:53:37 +0000

    day3

--- day_03.py
+++ day_03.py
@@ -9,8 +9,8 @@ def part_1():
 
     scores = []
     for line in lines:
-        second_half_set = set(line[int(len(line)/2):])
-        for char in line[:int(len(line)/2)]:
+        second_half_set = set(line[int(len(line) / 2) :])
+        for char in line[: int(len(line) / 2)]:
             if char in second_half_set:
                 scores.append(score_dict[char])
                 break
@@ -19,18 +19,21 @@ def part_1():
 
 
 def part_2():
-    def chunk(lst: list, n:int):
+    def chunk(lst: list, n: int):
         for i in range(0, len(lst), n):
-            yield lst[i:i + n]
+            yield lst[i : i + n]
 
     with open("day_03.txt") as f:
         lines = [[char for char in line.strip()] for line in f.readlines()]
 
     common_letters = []
     for line_1, line_2, line_3 in chunk(lines, 3):
-        common_letters.append(next(set(line_1).intersection(set(line_2)).intersection(line_3).__iter__()))
+        common_letters.append(
+            next(set(line_1).intersection(set(line_2)).intersection(line_3).__iter__())
+        )
 
     print(sum((score_dict[c] for c in common_letters)))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     part_2()
